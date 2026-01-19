65b615f11b6df1305d0e1f697cabf0da71870238
Dave <itschristiandavevilanofficial@gmail.com>
2026-01-20 03:07:08 +0800

    chore: deleted unused drizzle file

--- backend-ts/drizzle/0001_add_grade_to_submissions.sql
+++
@@ -1,6 +0,0 @@
-    -- Add grade column to submissions table
-    -- This column stores the auto-calculated grade based on test case results
-    ALTER TABLE submissions ADD COLUMN IF NOT EXISTS grade INTEGER;
-
-    -- Comment: Grade is calculated as floor((passed_tests / total_tests) * total_score)
-    -- where total_score comes from the assignment's totalScore field
